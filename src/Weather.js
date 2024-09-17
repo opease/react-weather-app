5736a891914187c9ed3ce8939c9dd9cbc4088a5e
opease <166313740+opease@users.noreply.github.com>
2024-09-17 19:16:47 +0200

    added axios with integration

--- src/Weather.js
+++ src/Weather.js
@@ -1,31 +1,93 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Weather.css";
+import axios from "axios";
 
-export default function Weather() {
-  return (
-    <div className="Weather">
-      <form>
-        <input type="text" className="form-input" placeholder="Enter a city" />
-        <input type="submit" value="Search" className="form-button" />
-      </form>
-      <div className="form"></div>
-      <div className="row">
-        <div className="col-8 mt-4">
-          <h1>San Francisco</h1>
-          <ul>
-            <li>Sunday 12:52, overcast clouds</li>
-            <li>
-              Humidity: <span className="humidity">91%</span>, Wind:{" "}
-              <span className="windspeed">8.05km/h</span>
-            </li>
-          </ul>
+export default function Weather(props) {
+  let [weatherData, setweatherData] = useState({ ready: false });
+
+  function handleResponse(response) {
+    console.log(response.data);
+
+    setweatherData({
+      ready: true,
+      temperature: response.data.temperature.current,
+      city: response.data.city,
+      humidity: response.data.temperature.humidity,
+      wind: response.data.wind.speed,
+      time: response.data.time,
+      description: response.data.condition.description,
+      iconUrl: response.data.condition.icon_url,
+    });
+  }
+
+  if (weatherData.ready) {
+    return (
+      <div className="Weather">
+        <form>
+          <input
+            type="text"
+            className="form-input"
+            placeholder="Enter a city"
+          />
+          <input type="submit" value="Search" className="form-button" />
+        </form>
+        <div className="row">
+          <div className="col-7 mt-4">
+            <h1>{weatherData.city}</h1>
+            <ul>
+              <li>Sunday 12:52, {weatherData.description}</li>
+              <li>
+                Humidity:{" "}
+                <span className="humidity">{weatherData.humidity}%</span>, Wind:{" "}
+                <span className="windspeed">{weatherData.wind}km/h</span>
+              </li>
+            </ul>
+          </div>
+          <div className="col-5">
+            <span className="icon">
+              <img src={weatherData.iconUrl} alt="icon" />
+            </span>
+            <span className="temperature">
+              {Math.round(weatherData.temperature)}
+            </span>
+            <span className="unit">°C</span>
+          </div>
         </div>
-        <div className="col-4">
-          <span className="icon">image</span>
-          <span className="temperature">13</span>
-          <span className="unit">°C</span>
-        </div>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://github.com/opease"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Odille Pease
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/opease/react-weather-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://react-weatherapp-opease.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            hosted on Netlify
+          </a>
+        </footer>
       </div>
-    </div>
-  );
+    );
+  } else {
+    let city = "Harare";
+    let apiKey = "1a3fca45557083c6198e4bt6d7cf4o1c";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+
+    axios.get(apiUrl).then(handleResponse);
+
+    return "Loading...";
+  }
 }
