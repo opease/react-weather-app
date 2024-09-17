a1cfa891ccb8a2803a97b98ebaff0982366f4ba3
opease <166313740+opease@users.noreply.github.com>
2024-09-17 17:52:21 +0200

    UI fixes - form placement and added logo and minor formatting (humidity, windspeed)

--- src/Weather.js
+++ src/Weather.js
@@ -4,7 +4,28 @@ import "./Weather.css";
 export default function Weather() {
   return (
     <div className="Weather">
-       
+      <form>
+        <input type="text" className="form-input" placeholder="Enter a city" />
+        <input type="submit" value="Search" className="form-button" />
+      </form>
+      <div className="form"></div>
+      <div className="row">
+        <div className="col-8 mt-4">
+          <h1>San Francisco</h1>
+          <ul>
+            <li>Sunday 12:52, overcast clouds</li>
+            <li>
+              Humidity: <span className="humidity">91%</span>, Wind:{" "}
+              <span className="windspeed">8.05km/h</span>
+            </li>
+          </ul>
+        </div>
+        <div className="col-4">
+          <span className="icon">image</span>
+          <span className="temperature">13</span>
+          <span className="unit">°C</span>
+        </div>
+      </div>
     </div>
   );
 }
